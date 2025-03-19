9093c6cc2779c1a968fb3aa4907cb11f3a12a36a
Justus <jades@xyz.com>
2025-03-19 08:40:55 +0300

    numbers.python

---
+++ number_functions.py
@@ -0,0 +1,22 @@
+import math
+
+x = 81
+print(math.sqrt(x))
+print(math.pi)
+print(math.pi)
+print(math.floor(78.87))
+print(math.ceil(78.67))
+
+radius =54
+height =24
+
+volume =22/7 *radius* height
+volume =round(volume,2)
+print(volume)
+
+sa = 2+22/7*radius*(radius +height)
+sa=round(sa,2)
+print(sa)
+
+results =10 +20 /4 +6 - 3 *(3+2)
+print(results)
