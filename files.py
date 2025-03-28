abb87ee1806219017e13f0d172ee9bd07670e374
Justus <jades@xyz.com>
2025-03-28 08:22:51 +0300

    files

--- files.py
+++ files.py
@@ -1,6 +1,6 @@
 file = open('test.txt', 'w') #to repeat= 'a' then \n (in 'here')
 
-file.write("mash cool")   #(here)
+file.write("mash cool ")   #(here)
 file.close()
 
 try:
@@ -30,13 +30,3 @@ print(pp)
 print(name)
 
 
-import csv
-
-with open('profiles2.csv','w',newline='') as file:
-    writer = csv.writer(file)
-    row_list=[["name","age","country"],
-             ["Ethan Justus","23","Kenya"],
-             ["Cindy Marks","22","Somalia"],
-             ["Mike Dean", "31","England"],]
-
-    writer.writerows(row_list)
--- txt_to_img.py
+++ txt_to_img.py
@@ -1,19 +1,19 @@
-from PIL import Image,ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
-img = Image.open('Images/img cover.jpg')
+img = Image.open('images/img cover.jpg')
 draw = ImageDraw.Draw(Image)
 
 font = ImageFont.truetype('arial.ttf', 20)
 
 text = "ForestView"
 
-bbox = draw.textbbox((0,0), text, font=font)
+bbox = draw.textbbox((0, 0), text, font=font)
 text_width = bbox[2] - bbox[0]
 text_height = bbox[3] - bbox[1]
 image_width, image_height = img.size
 x = (image_width - text_width) / 2
 y = (image_height - text_height) / 2
 
-draw.text((x, y), text,fill = "white", font = font)
+draw.text((x, y), text, fill="white", font=font)
 img.show()
-img.save('Images/img cover.jpg')
+img.save('images/img cover.jpg')
